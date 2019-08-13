a8701864a116f0b9b668da7587ba6f91238c5fcf
Aziko13 <aziz.fizmat@gmail.com>
2019-08-13 18:16:17 +0800

    init commit

---
+++ src/DataPrep.py
@@ -0,0 +1,155 @@
+from PIL import Image, ImageDraw
+import os
+from glob import glob
+import json
+import numpy as np
+import keras
+import cv2
+
+
+def get_mask(img, mask_data):
+
+    x = mask_data.get('regions')[0].get('shape_attributes').get('all_points_x')
+    y = mask_data.get('regions')[0].get('shape_attributes').get('all_points_y')
+    mask_img = Image.fromarray(np.zeros(list(reversed(img.size)), dtype=np.uint8))
+    draw = ImageDraw.Draw(mask_img)
+    draw.polygon(list(zip(x, y)), fill="white")
+
+    return mask_img
+
+
+def get_resized_data(path_out, imgs, masks_json, size):
+
+    cmd = 'rm -rf {0}'.format(os.path.join(path_out, 'images_' + str(size)))
+    print(cmd)
+    os.system(cmd)
+    cmd = 'mkdir {0}'.format(os.path.join(path_out, 'images_' + str(size)))
+    print(cmd)
+    os.system(cmd)
+
+    cmd = 'rm -rf {0}'.format(os.path.join(path_out, 'masks_' + str(size)))
+    os.system(cmd)
+    cmd = 'mkdir {0}'.format(os.path.join(path_out, 'masks_' + str(size)))
+    os.system(cmd)
+
+
+    for img_path in imgs:
+        img_name = img_path.split('/')[-1]
+        mask_data = masks_json.get('_via_img_metadata').get(img_name)
+        img = Image.open(img_path)
+        mask = get_mask(img, mask_data)
+
+        img = img.resize((size, size))
+        mask = mask.resize((size, size))
+
+        img.save(os.path.join(os.path.join(path_out, 'images_' + str(size) + '/' + img_name)))
+        mask.save(os.path.join(os.path.join(path_out, 'masks_' + str(size) + '/' + img_name)))
+
+
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+
+    def __init__(self, imgs_path, masks_path,
+                 augmentations=None, batch_size=16, img_size=512, n_channels=3, shuffle=True):
+        'Initialization'
+        self.batch_size = batch_size
+        self.train_im_paths = glob(imgs_path + '/*')
+
+        self.train_im_path = imgs_path
+        self.train_mask_path = masks_path
+
+        self.img_size = img_size
+
+        self.n_channels = n_channels
+        self.shuffle = shuffle
+        self.augment = augmentations
+        self.on_epoch_end()
+
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.ceil(len(self.train_im_paths) / self.batch_size))
+
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index * self.batch_size:min((index + 1) * self.batch_size, len(self.train_im_paths))]
+
+        # Find list of IDs
+        list_IDs_im = [self.train_im_paths[k] for k in indexes]
+
+        # Generate data
+        X, y = self.data_generation(list_IDs_im)
+
+        if self.augment is None:
+            return X, np.array(y) / 255
+        else:
+            im, mask = [], []
+            for x, y in zip(X, y):
+                augmented = self.augment(image=x, mask=y)
+                im.append(augmented['image'])
+                mask.append(augmented['mask'])
+            return np.array(im), np.array(mask) / 255
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.train_im_paths))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def data_generation(self, list_IDs_im):
+        'Generates data containing batch_size samples'  # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((len(list_IDs_im), self.img_size, self.img_size, self.n_channels))
+        y = np.empty((len(list_IDs_im), self.img_size, self.img_size, 1))
+
+        # Generate data
+        for i, im_path in enumerate(list_IDs_im):
+
+            im = np.array(Image.open(im_path))
+            mask_path = im_path.replace(self.train_im_path, self.train_mask_path)
+
+            mask = np.array(Image.open(mask_path))
+
+            if len(im.shape) == 2:
+                im = np.repeat(im[..., None], 3, 2)
+
+            #             # Resize sample
+            X[i,] = cv2.resize(im, (self.img_size, self.img_size))
+
+            # Store class
+            y[i,] = cv2.resize(mask, (self.img_size, self.img_size))[..., np.newaxis]
+            y[y > 0] = 255
+
+        return np.uint8(X), np.uint8(y)
+
+
+if __name__ == '__main__':
+
+    # ---------- Testing images resizing and masks preparation
+    # input_path = '/home/aziz/Documents/Cars_Proj/input/data/'
+    # path_out = '/home/aziz/Documents/Cars_Proj/input/resized/train'
+    # size = 512
+    #
+    # train = sorted(glob(os.path.join(input_path, 'train/*.jpeg')))
+    # val = sorted(glob(os.path.join(input_path, 'val/*.jpeg')))
+    #
+    # print(len(train), len(val))
+    #
+    # json_file = open(os.path.join(input_path, 'train.json'))
+    # train_masks_json = json.load(json_file)
+    #
+    # json_file = open(os.path.join(input_path, 'val.json'))
+    # val_masks_json = json.load(json_file)
+    #
+    # get_resized_data(path_out, train, train_masks_json, size)
+    # -------------------------------------------------------------
+
+    # Testing data generator
+    train_im_path = '/home/aziz/Documents/Cars_Proj/input/resized/train/images_512'
+    train_mask_path = '/home/aziz/Documents/Cars_Proj/input/resized/train/masks_512'
+
+    a = DataGenerator(imgs_path=train_im_path, masks_path=train_mask_path, augmentations=None,
+                      batch_size=16, img_size=512, n_channels=3, shuffle=False)
+    images, masks = a.__getitem__(1)
+    print(images.shape, masks.shape)
+    # -------------------------------------------------------------
---
+++ src/Unet_1.py
@@ -0,0 +1,108 @@
+from keras import backend as K
+from keras.layers import Input, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate, BatchNormalization
+from keras.layers.core import Dropout, Lambda
+from keras import Model
+import numpy as np
+import tensorflow as tf
+from DataPrep import DataGenerator
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
+
+def mean_iou(y_true, y_pred):
+    prec = []
+    for t in np.arange(0.5, 1.0, 0.05):
+        y_pred_ = tf.to_int32(y_pred > t)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([up_opt]):
+            score = tf.identity(score)
+        prec.append(score)
+    return K.mean(K.stack(prec), axis=0)
+
+def get_model(size):
+    inputs = Input((size, size, 3))
+    s = Lambda(lambda x: x / 255) (inputs)
+
+    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (s)
+    c1 = Dropout(0.1) (c1)
+    c1 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c1)
+    p1 = MaxPooling2D((2, 2)) (c1)
+
+    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p1)
+    c2 = Dropout(0.1) (c2)
+    c2 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c2)
+    p2 = MaxPooling2D((2, 2)) (c2)
+
+    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p2)
+    c3 = Dropout(0.2) (c3)
+    c3 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c3)
+    p3 = MaxPooling2D((2, 2)) (c3)
+
+    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p3)
+    c4 = Dropout(0.2) (c4)
+    c4 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c4)
+    p4 = MaxPooling2D(pool_size=(2, 2)) (c4)
+
+    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (p4)
+    c5 = Dropout(0.3) (c5)
+    c5 = Conv2D(256, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c5)
+
+    u6 = Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same') (c5)
+    u6 = concatenate([u6, c4])
+    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u6)
+    c6 = Dropout(0.2) (c6)
+    c6 = Conv2D(128, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c6)
+
+    u7 = Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same') (c6)
+    u7 = concatenate([u7, c3])
+    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u7)
+    c7 = Dropout(0.2) (c7)
+    c7 = Conv2D(64, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c7)
+
+    u8 = Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same') (c7)
+    u8 = concatenate([u8, c2])
+    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u8)
+    c8 = Dropout(0.1) (c8)
+    c8 = Conv2D(32, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c8)
+
+    u9 = Conv2DTranspose(16, (2, 2), strides=(2, 2), padding='same') (c8)
+    u9 = concatenate([u9, c1], axis=3)
+    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (u9)
+    c9 = Dropout(0.1) (c9)
+    c9 = Conv2D(16, (3, 3), activation='elu', kernel_initializer='he_normal', padding='same') (c9)
+
+    outputs = Conv2D(1, (1, 1), activation='sigmoid') (c9)
+
+    model = Model(inputs=[inputs], outputs=[outputs])
+    return model
+
+
+if __name__ == '__main__':
+
+    train_im_path = '/home/aziz/Documents/Cars_Proj/input/resized/train/images_512'
+    train_mask_path = '/home/aziz/Documents/Cars_Proj/input/resized/train/masks_512'
+    val_im_path = '/home/aziz/Documents/Cars_Proj/input/resized/val/images_512'
+    val_mask_path = '/home/aziz/Documents/Cars_Proj/input/resized/val/masks_512'
+    size = 512
+
+
+    model = get_model(size)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+    training_generator = DataGenerator(imgs_path=train_im_path, masks_path=train_mask_path, augmentations=None,
+                                       batch_size=4, img_size=512, n_channels=3, shuffle=False)
+
+    validation_generator = DataGenerator(imgs_path=val_im_path, masks_path=val_mask_path, augmentations=None,
+                                         batch_size=4, img_size=512, n_channels=3, shuffle=False)
+
+
+    early_stopping = EarlyStopping(patience=10, verbose=1)
+    model_checkpoint = ModelCheckpoint('/home/aziz/Documents/Cars_Proj/models/unet_1.h5', verbose=1, save_best_only=True)
+
+    history = model.fit_generator(generator=training_generator,
+                                  validation_data=validation_generator,
+                                  epochs=10,
+                                  verbose=2,
+                                  callbacks=[early_stopping, model_checkpoint],
+                                  shuffle=True)
+
